b94ebb9e95813184c880f24b571573477a6efe24
5percent <v@luzhe.info>
2016-08-23 13:23:24 +0800

    up

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ var dust = require('dustjs-linkedin');
 
 module.exports = function(content, file, conf){
 	var name = conf.basePath ? conf.basePath + file.filename : file.subpathNoExt;
-    return "define('" + file.id + "', function () {\n\t" + dust.compile(content.toString(), name) + "\n});";
+    return "define('" + file.getId() + "', function (require, exports, module) {\n\t" + dust.compile(content.toString(), name) + "\n});";
 };
